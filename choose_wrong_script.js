6ebe00ae4ad07ac2e238f934fd69b78cd94b325d
Hybrid <oukinme@outlook.com>
2024-11-01 16:25:26 +0900

    Update choose_wrong_script.js

--- choose_wrong_script.js
+++ choose_wrong_script.js
@@ -21,8 +21,8 @@ var challenge=document.getElementById("challenge");
 var h1_list = ["Қосымшаны таңдау"];
 var h1_list_2 = ["Выбери лишнее"];
 var h1_tags = document.getElementsByTagName("h1");
-var correct_answer = new Audio("../../correct_answer.mp3");
-var wrong_answer = new Audio("../../wrong_answer.mp3");
+var correct_answer = new Audio("correct_answer.mp3");
+var wrong_answer = new Audio("wrong_answer.mp3");
 
 
 
@@ -129,7 +129,7 @@ var on_image_4_click = function(){
 }
 var on_cancel_click=function(){
     var a=0;
-    window.location.href = "file:///C:/Programming/webDev/games_website/index.html";
+    window.location.href = "index.html";
 }
 
 
